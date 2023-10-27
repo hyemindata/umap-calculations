407d1329b3117220c2f908119f55946f185c2ab8
hyemindata <hyemin@refikanadolstudio.com>
2023-10-27 14:34:58 -0700

    Add command line argument and grid search

--- calculate_umap.py
+++ calculate_umap.py
@@ -1,8 +1,13 @@
-from cuml.manifold.umap import UMAP
-import numpy as np
 import os
 import gc
+import json
+import argparse
+from datetime import datetime
+from pathlib import Path
+
 import torch
+import numpy as np
+from cuml.manifold.umap import UMAP
 
 gc.collect()
 torch.cuda.empty_cache()
@@ -12,36 +17,69 @@ def read_npy(data_path):
     print("Data loaded.", data.shape)
     return data
 
-
-def calculate_umap(data, n_neighbors=15):
-    parameters = {"min_dist": 0.1, "spread": 1.0}
+def calculate_umap(data, umap_params):
     print("UMAP calculation has started.")
-    result = UMAP(
-        n_components=6,
-        n_neighbors=n_neighbors,
-        min_dist=parameters["min_dist"],
-        spread=parameters["spread"],
-    ).fit_transform(data)
+    print(umap_params)
+    result = UMAP(**umap_params).fit_transform(data)
     return result
 
+def save_embeddings(save_path, umap_embeddings):
+    np.save(save_path, umap_embeddings)
+    print(f"Saved to {save_path}")
 
-def save_embeddings(data_path, umap_embeddings):
-    output_folder = "umap"
-    os.makedirs(output_folder, exist_ok=True)
-    output_path = os.path.join(
-        output_folder, os.path.splitext(os.path.basename(data_path))[0] + "_umap.npy"
-    )
-    np.save(output_path, umap_embeddings)
-    print(f"Saved to {output_path}")
+def get_timestamp():
+    timestamp = datetime.now().strftime("%d%m%Y-%H%M%S")
+    return timestamp
 
+def prepare_save_path(data_path, idx, save_dir, timestamp):
+    title = Path(data_path).stem
+    umap_title = f'{title}_{timestamp}'
+
+    exp_title = f'{umap_title}_{idx:05d}'
+
+    umap_save_dir = Path(save_dir) / umap_title / exp_title
+    os.makedirs(umap_save_dir, exist_ok=True)
+
+    umap_save_path = umap_save_dir / (exp_title+'.npy')
+    config_save_path = umap_save_dir / (exp_title+'.json')
+    return umap_save_path, config_save_path
+
+def save_config(save_path, umap_params):
+    with open(save_path, 'w') as f:
+        json.dump(umap_params, f)
+    
+def main(args):
+    timestamp = get_timestamp()
+    for data_path in args.pca:
+        data = read_npy(data_path)
+
+        idx = 0
+        for nn in args.n_neighbors:
+            for md in args.min_dist:
+                for sp in args.spread:
+                    for mt in args.metric:
+                        for ns in args.negative_sample_rate:
+                            for rs in args.repulsion_strength:
+                                umap_params = {"n_components" : 6, "n_neighbors" : nn,
+                                               "min_dist" : md, "spread" : sp, "metric" :mt,
+                                               "negative_sample_rate" : ns, "repulsion_strength" : rs}
+                                umap_embeddings = calculate_umap(data, umap_params=umap_params)
+                                umap_path, conf_path = prepare_save_path(data_path, idx, args.output, timestamp)
+                                save_embeddings(umap_path, umap_embeddings)
+                                save_config(conf_path, umap_params)
+                                idx += 1
 
 if __name__ == "__main__":
-    data_paths = [
-        "/mnt/c/Users/Mert/Desktop/florfaunea/pca/sample_fauna_embeddings_cleaned_pca.npy",
-        "/mnt/c/Users/Mert/Desktop/florfaunea/pca/sample_flora_embeddings_cleaned_pca.npy",
-        "/mnt/c/Users/Mert/Desktop/florfaunea/pca/sample_fungi_embeddings_cleaned_pca.npy",
-    ]
-    for data_path in data_paths:
-        data = read_npy(data_path)
-        umap_embeddings = calculate_umap(data)
-        save_embeddings(data_path, umap_embeddings)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--pca', type=str, nargs='*', help='Path to PCA npy')
+    parser.add_argument('--n_neighbors', type=int, nargs='*', default=[12])
+    parser.add_argument('--min_dist', type=float, nargs='*', default=[0.01])
+    parser.add_argument('--spread', type=float, nargs='*', default=[1.0])
+    parser.add_argument('--metric', type=str, nargs='*', default=["euclidean"])
+    parser.add_argument('--negative_sample_rate', type=int, nargs='*', default=[5])
+    parser.add_argument('--repulsion_strength', type=float, nargs='*', default=[1.0])
+
+    parser.add_argument('--output', type=str, default='./output', help='Umap save directory')
+    args = parser.parse_args()
+
+    main(args)
